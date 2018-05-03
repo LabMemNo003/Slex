a253489cbea3585a82f0c980d4439aa1f66dc29a
LabMemNo003 <LabMemNo003@outlook.com>
2018-05-04 00:12:24 +0800

    Add "YY_" to avoid collision in main().

--- Slex/main.cpp
+++ Slex/main.cpp
@@ -9,79 +9,79 @@ using namespace std;
 //============================== T E S T ==============================
 
 int main() {
-    string patterns[21];
-    patterns[0] = "\\+";
-    patterns[1] = "\\-";
-    patterns[2] = "\\*";
-    patterns[3] = "/";
-    patterns[4] = ">";
-    patterns[5] = "<";
-    patterns[6] = "=";
-    patterns[7] = "\\(";
-    patterns[8] = "\\)";
-    patterns[9] = ";";
-    patterns[10] = "if";
-    patterns[11] = "then";
-    patterns[12] = "else";
-    patterns[13] = "while";
-    patterns[14] = "do";
-    patterns[15] = "[a-zA-Z][a-zA-Z0-9]*";
-    patterns[16] = "0|[1-9][0-9]*";
-    patterns[17] = "0[0-7]+";
-    patterns[18] = "0x[0-9a-fA-F]+";
-    patterns[19] = "[\\ \\n\\f\\r\\t\\v]";
-    patterns[20] = ".";
+    string YY_patterns[21];
+    YY_patterns[0] = "\\+";
+    YY_patterns[1] = "\\-";
+    YY_patterns[2] = "\\*";
+    YY_patterns[3] = "/";
+    YY_patterns[4] = ">";
+    YY_patterns[5] = "<";
+    YY_patterns[6] = "=";
+    YY_patterns[7] = "\\(";
+    YY_patterns[8] = "\\)";
+    YY_patterns[9] = ";";
+    YY_patterns[10] = "if";
+    YY_patterns[11] = "then";
+    YY_patterns[12] = "else";
+    YY_patterns[13] = "while";
+    YY_patterns[14] = "do";
+    YY_patterns[15] = "[a-zA-Z][a-zA-Z0-9]*";
+    YY_patterns[16] = "0|[1-9][0-9]*";
+    YY_patterns[17] = "0[0-7]+";
+    YY_patterns[18] = "0x[0-9a-fA-F]+";
+    YY_patterns[19] = "[\\ \\n\\f\\r\\t\\v]";
+    YY_patterns[20] = ".";
 
-	YY_Re automas[21];
-    for (int i = 0; i < 21; i++) {
-        automas[i].DoIt(patterns[i]);
+	YY_Re YY_automas[21];
+    for (int YY_i = 0; YY_i < 21; YY_i++) {
+        YY_automas[YY_i].DoIt(YY_patterns[YY_i]);
     }
 
-    typedef void(*PtrFunc)(string);
-    PtrFunc actions[21];
-    actions[0] = [](string str) { cout << "+" << endl; };
-    actions[1] = [](string str) { cout << "-" << endl; };
-    actions[2] = [](string str) { cout << "*" << endl; };
-    actions[3] = [](string str) { cout << "/" << endl; };
-    actions[4] = [](string str) { cout << ">" << endl; };
-    actions[5] = [](string str) { cout << "<" << endl; };
-    actions[6] = [](string str) { cout << "=" << endl; };
-    actions[7] = [](string str) { cout << "(" << endl; };
-    actions[8] = [](string str) { cout << ")" << endl; };
-    actions[9] = [](string str) { cout << ";" << endl; };
-    actions[10] = [](string str) { cout << "IF" << endl; };
-    actions[11] = [](string str) { cout << "THEN" << endl; };
-    actions[12] = [](string str) { cout << "ELSE" << endl; };
-    actions[13] = [](string str) { cout << "WHILE" << endl; };
-    actions[14] = [](string str) { cout << "DO" << endl; };
-    actions[15] = [](string str) { cout << "IDN\t" << str << endl; };
-    actions[16] = [](string str) { cout << "INT10\t" << str << endl; };
-    actions[17] = [](string str) { cout << "INT8\t" << str << endl; };
-    actions[18] = [](string str) { cout << "INT16\t" << str << endl; };
-    actions[19] = [](string str) { };
-    actions[20] = [](string str) {cout << "ERROR" << endl; };
+    typedef void(*YY_PtrFunc)(string);
+    YY_PtrFunc YY_actions[21];
+    YY_actions[0] = [](string str) { cout << "+" << endl; };
+    YY_actions[1] = [](string str) { cout << "-" << endl; };
+    YY_actions[2] = [](string str) { cout << "*" << endl; };
+    YY_actions[3] = [](string str) { cout << "/" << endl; };
+    YY_actions[4] = [](string str) { cout << ">" << endl; };
+    YY_actions[5] = [](string str) { cout << "<" << endl; };
+    YY_actions[6] = [](string str) { cout << "=" << endl; };
+    YY_actions[7] = [](string str) { cout << "(" << endl; };
+    YY_actions[8] = [](string str) { cout << ")" << endl; };
+    YY_actions[9] = [](string str) { cout << ";" << endl; };
+    YY_actions[10] = [](string str) { cout << "IF" << endl; };
+    YY_actions[11] = [](string str) { cout << "THEN" << endl; };
+    YY_actions[12] = [](string str) { cout << "ELSE" << endl; };
+    YY_actions[13] = [](string str) { cout << "WHILE" << endl; };
+    YY_actions[14] = [](string str) { cout << "DO" << endl; };
+    YY_actions[15] = [](string str) { cout << "IDN\t" << str << endl; };
+    YY_actions[16] = [](string str) { cout << "INT10\t" << str << endl; };
+    YY_actions[17] = [](string str) { cout << "INT8\t" << str << endl; };
+    YY_actions[18] = [](string str) { cout << "INT16\t" << str << endl; };
+    YY_actions[19] = [](string str) { };
+    YY_actions[20] = [](string str) { cout << "ERROR" << endl; };
 
 
-    string input = "0  92+data>  0x3f  00  while";
-    cout << "Input string is:" << endl << endl << input << endl;
+    string YY_input = "0  92+data>  0x3f  00  while";
+    cout << "Input string is:" << endl << endl << YY_input << endl;
 
     cout << endl;
 
     cout << "The result is:" << endl << endl;
-    int ind = 0;
-    int len = input.length();
-    while (ind < len) {
-        int max_match = 0;
-        int max_pattern = 0;
-        for (int i = 0; i < 21; i++) {
-            int len_match = automas[i].Match(input.substr(ind));
-            if (len_match > max_match) {
-                max_pattern = i;
-                max_match = len_match;
+    int YY_ind = 0;
+    int YY_len = YY_input.length();
+    while (YY_ind < YY_len) {
+        int YY_max_match = 0;
+        int YY_max_pattern = 0;
+        for (int YY_i = 0; YY_i < 21; YY_i++) {
+            int YY_len_match = YY_automas[YY_i].Match(YY_input.substr(YY_ind));
+            if (YY_len_match > YY_max_match) {
+                YY_max_pattern = YY_i;
+                YY_max_match = YY_len_match;
             }
         }
-        actions[max_pattern](input.substr(ind, max_match));
-        ind = ind + max_match;
+        YY_actions[YY_max_pattern](YY_input.substr(YY_ind, YY_max_match));
+        YY_ind = YY_ind + YY_max_match;
     }
     cout << endl;
 
